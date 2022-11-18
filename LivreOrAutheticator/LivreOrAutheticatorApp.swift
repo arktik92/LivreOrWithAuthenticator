aa9565d4eba4cb4789d0643a46b0092917a24127
arktik92 <esteban.semellier@gmail.com>
2022-11-18 10:02:11 +0100

    Initial Commit

---
+++ LivreOrAutheticator/LivreOrAutheticatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivreOrAutheticatorApp.swift
+//  LivreOrAutheticator
+//
+//  Created by Esteban SEMELLIER on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LivreOrAutheticatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
